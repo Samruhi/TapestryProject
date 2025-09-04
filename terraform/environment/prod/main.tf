5571010c9307ef8110609e162633ea3f65fea40e
Samruhi <105702602+Samruhi@users.noreply.github.com>
2025-09-04 21:19:53 +0530

    Update main.tf

--- terraform/environment/prod/main.tf
+++ terraform/environment/prod/main.tf
@@ -1,6 +1,6 @@
 # EC2 Instance
 resource "aws_instance" "prod_ec2" {
-  ami           = "ami-08e5424edfe926b43" # Amazon Linux 2 (replace with your region's AMI)
+  ami           = "ami-03aa99ddf5498ceb9" # Amazon Linux 2 (replace with your region's AMI)
   instance_type = var.instance_type
 
   #iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
