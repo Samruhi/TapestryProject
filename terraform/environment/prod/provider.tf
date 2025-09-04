110282c1fef772a08da4145585dabee04d0b223f
Samruhi <105702602+Samruhi@users.noreply.github.com>
2025-09-04 21:36:40 +0530

    Update provider.tf

--- terraform/environment/prod/provider.tf
+++ terraform/environment/prod/provider.tf
@@ -19,6 +19,6 @@ terraform {
 
 provider "aws" {
     region = "us-west-2"
-    access_key = "AKIA5K7ZA3MXFN2DB275"
-    secret_key = "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"
+    access_key = "AKIA5K7ZA3MXMRHMS4MZ"
+    secret_key = "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"
 }
